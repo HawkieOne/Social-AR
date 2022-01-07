7047ea430bc82b9e208c20693d370b5bdfbe453e
Restyled.io <commits@restyled.io>
2022-01-07 16:16:00 +0000

    Restyled by clang-format

--- Assets/Scripts/PlacementObject.cs
+++ Assets/Scripts/PlacementObject.cs
@@ -1,73 +1,52 @@
 using TMPro;
 using UnityEngine;
 
-public class PlacementObject : MonoBehaviour
-{
-    [SerializeField]
-    private bool IsSelected;
+public class PlacementObject : MonoBehaviour {
+  [SerializeField]
+  private bool IsSelected;
 
-    [SerializeField]
-    private bool IsLocked;
+  [SerializeField]
+  private bool IsLocked;
 
-    public bool Selected
-    {
-        get
-        {
-            return this.IsSelected;
-        }
-        set
-        {
-            IsSelected = value;
-        }
+  public bool Selected {
+    get { return this.IsSelected; }
+    set { IsSelected = value; }
+  }
+
+  public bool Locked {
+    get { return this.IsLocked; }
+    set { IsLocked = value; }
+  }
+
+  [SerializeField]
+  private TextMeshPro OverlayText;
+
+  [SerializeField]
+  private Canvas canvasComponent;
+
+  [SerializeField]
+  private string OverlayDisplayText;
+
+  public void SetOverlayText(string text) {
+    if (OverlayText != null) {
+      OverlayText.gameObject.SetActive(true);
+      OverlayText.text = text;
     }
+  }
 
-    public bool Locked
-    {
-        get
-        {
-            return this.IsLocked;
-        }
-        set
-        {
-            IsLocked = value;
-        }
+  void Awake() {
+    OverlayText = GetComponentInChildren<TextMeshPro>();
+    if (OverlayText != null) {
+      OverlayText.gameObject.SetActive(false);
     }
+  }
 
-    [SerializeField]
-    private TextMeshPro OverlayText;
+  public void ToggleOverlay() {
+    OverlayText.gameObject.SetActive(IsSelected);
+    OverlayText.text = OverlayDisplayText;
+  }
 
-    [SerializeField]
-    private Canvas canvasComponent;
-
-    [SerializeField]
-    private string OverlayDisplayText;
-
-    public void SetOverlayText(string text)
-    {
-        if (OverlayText != null)
-        {
-            OverlayText.gameObject.SetActive(true);
-            OverlayText.text = text;
-        }
-    }
-
-    void Awake()
-    {
-        OverlayText = GetComponentInChildren<TextMeshPro>();
-        if (OverlayText != null)
-        {
-            OverlayText.gameObject.SetActive(false);
-        }
-    }
-
-    public void ToggleOverlay()
-    {
-        OverlayText.gameObject.SetActive(IsSelected);
-        OverlayText.text = OverlayDisplayText;
-    }
-
-    public void ToggleCanvas()
-    {
-        canvasComponent?.gameObject.SetActive(IsSelected);
-    }
+  public void ToggleCanvas() {
+    canvasComponent?.gameObject.SetActive(IsSelected);
+  }
 }
